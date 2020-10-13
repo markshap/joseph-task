a734eba314f462a15cb1a131b51144973b01c631
Mark Shapiro <mark.shapiro@axonvibe.com>
2020-10-13 17:37:27 +0300

    add project files

---
+++ src/main/java/com/markshapiro/josephtask/Main.java
@@ -0,0 +1,41 @@
+package com.markshapiro.josephtask;
+
+import static org.junit.Assert.assertEquals;
+
+public class Main {
+
+    private static boolean survives(final int pos, final int N, final int M, final int shift) {
+        if (M == 1 || N == 1) {
+            // special case. The last one survives
+            return pos == N;
+        } else if ((pos + shift) % M == 0) {
+            // sorry, man
+            return false;
+        }  else {
+            // the next N is reduced by the number of killed
+            int nextN = N - (N + shift)  / M;
+            // the next Pos is reduced by the number of killed in front of the current pos
+            int nextPos = pos - (pos + shift)  / M;
+            // the next shift is the old one plus the reminder
+            int nextShift = (shift + N % M) % M;
+            return survives(nextPos, nextN, M, nextShift);
+        }
+    }
+
+    private static int josephTask(final int N, final int M) {
+        for (int i = 1; i <= N; i++) {
+            if (survives(i, N, M, 0)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+     public static void main(String[] args) {
+         assertEquals(1, josephTask(1, 1));
+         assertEquals(2, josephTask(2, 1));
+         assertEquals(50000000, josephTask(50000000, 1));
+         assertEquals(3, josephTask(8, 5));
+         assertEquals(28, josephTask(40, 3));
+    }
+}
