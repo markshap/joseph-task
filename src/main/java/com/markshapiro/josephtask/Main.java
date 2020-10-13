0095ba72fcb6e8996060d26ed45c90219348415a
Mark Shapiro <mark.shapiro@axonvibe.com>
2020-10-13 18:38:54 +0300

    non-recursive version

--- src/main/java/com/markshapiro/josephtask/Main.java
+++ src/main/java/com/markshapiro/josephtask/Main.java
@@ -4,27 +4,29 @@ import static org.junit.Assert.assertEquals;
 
 public class Main {
 
-    private static boolean survives(final int pos, final int N, final int M, final int shift) {
-        if (M == 1 || N == 1) {
-            // special case. The last one survives
-            return pos == N;
-        } else if ((pos + shift) % M == 0) {
-            // sorry, man
-            return false;
-        }  else {
+    private static boolean survives(final int pos, final int N, final int M) {
+        int p = pos;
+        int n = N;
+        int shift = 0;
+        while (n > 1 && M > 1) {
+            if ((p + shift) % M == 0) {
+                // sorry, man
+                return false;
+            };
             // the next N is reduced by the number of killed
-            int nextN = N - (N + shift)  / M;
+            int nextN = n - (n + shift)  / M;
             // the next Pos is reduced by the number of killed in front of the current pos
-            int nextPos = pos - (pos + shift)  / M;
+            p = p - (p + shift)  / M;
             // the next shift is the old one plus the reminder
-            int nextShift = (shift + N % M) % M;
-            return survives(nextPos, nextN, M, nextShift);
+            shift = (shift + n % M) % M;
+            n = nextN;
         }
+        return p == n;
     }
 
     private static int josephTask(final int N, final int M) {
         for (int i = 1; i <= N; i++) {
-            if (survives(i, N, M, 0)) {
+            if (survives(i, N, M)) {
                 return i;
             }
         }
@@ -35,6 +37,7 @@ public class Main {
          assertEquals(1, josephTask(1, 1));
          assertEquals(2, josephTask(2, 1));
          assertEquals(50000000, josephTask(50000000, 1));
+         assertEquals(50000000, josephTask(50000000, 4000000));
          assertEquals(3, josephTask(8, 5));
          assertEquals(28, josephTask(40, 3));
     }
