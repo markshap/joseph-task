157b2bcfbb50723618a58337ecd3e44dbedeed5e
Mark Shapiro <mark.shapiro@axonvibe.com>
2020-10-15 10:10:37 +0300

    using parallel stream

--- src/main/java/com/markshapiro/josephtask/Main.java
+++ src/main/java/com/markshapiro/josephtask/Main.java
@@ -14,11 +14,12 @@ public class Main {
             if ((p + shift) % M == 0) {
                 // sorry, man
                 return false;
-            };
+            }
+            ;
             // the next N is reduced by the number of killed
-            int nextN = n - (n + shift)  / M;
+            int nextN = n - (n + shift) / M;
             // the next Pos is reduced by the number of killed in front of the current pos
-            p = p - (p + shift)  / M;
+            p = p - (p + shift) / M;
             // the next shift is the old one plus the reminder
             shift = (shift + n % M) % M;
             n = nextN;
@@ -27,15 +28,15 @@ public class Main {
     }
 
     private static int josephTask(final int N, final int M) {
-        return IntStream.range(1, N+1).parallel().filter(i-> survives(i, N, M, 0)).findAny().orElse(-1);
+        return IntStream.range(1, N + 1).parallel().filter(i -> survives(i, N, M)).findAny().orElse(-1);
     }
 
-     public static void main(String[] args) {
-         assertEquals(1, josephTask(1, 1));
-         assertEquals(2, josephTask(2, 1));
-         assertEquals(50000000, josephTask(50000000, 1));
-         assertEquals(32891137, josephTask(50000000, 2));
-         assertEquals(3, josephTask(8, 5));
-         assertEquals(28, josephTask(40, 3));
+    public static void main(String[] args) {
+        assertEquals(1, josephTask(1, 1));
+        assertEquals(2, josephTask(2, 1));
+        assertEquals(50000000, josephTask(50000000, 1));
+        assertEquals(32891137, josephTask(50000000, 2));
+        assertEquals(3, josephTask(8, 5));
+        assertEquals(28, josephTask(40, 3));
     }
 }
